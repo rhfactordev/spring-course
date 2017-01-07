52e80d18fc81719864d6c78ef18acb4dece48af1
Roberto Fonseca <roberto@rhfactor.com.br>
2017-01-07 13:49:53 -0200

    Criar método para lsitar produtos

--- src/main/java/br/com/rhfactor/shop/controllers/ProdutoController.java
+++ src/main/java/br/com/rhfactor/shop/controllers/ProdutoController.java
@@ -3,6 +3,7 @@ package br.com.rhfactor.shop.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.rhfactor.shop.beans.Produto;
 import br.com.rhfactor.shop.daos.ProdutoDAO;
@@ -18,10 +19,17 @@ public class ProdutoController {
 		return "produto/form";
 	}
 	
-	@RequestMapping("/produto/salvar")
+	@RequestMapping(value="/produto/salvar")
 	public String salvar( Produto produto ){
 		this.produtoDao.salvar(produto);
 		return "produto/ok";
+	}
+	
+	@RequestMapping(value="/produtos")
+	public ModelAndView listar(){
+		ModelAndView modelAndView = new ModelAndView("/produtos/listar");
+		modelAndView.addObject("produtos",this.produtoDao.listar());
+		return modelAndView;
 	}
 
 }
